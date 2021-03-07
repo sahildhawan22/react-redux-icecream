fa29876a98990834d42339faee2346051d5471fa
Sahil Dhawan <sahildhawan22@gmail.com>
2021-03-07 15:19:51 +0530

    Ep-29

---
+++ src/components/UserContainer.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../redux/index";
+
+function UserContainer({ userData, fetchUsers }) {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  return userData.loading ? (
+    "Loading...."
+  ) : userData.error ? (
+    <h2>{userData.error}</h2>
+  ) : (
+    <div>
+      <h2>Users List</h2>
+      {userData &&
+        userData.users &&
+        userData.users.map((user) => <p>{user.name}</p>)}
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -4,6 +4,8 @@ import {
   FETCH_USERS_SUCCESS,
 } from "./userTypes";
 
+import axios from "axios";
+
 export const fetchUsersRequest = () => {
   return {
     type: FETCH_USERS_REQUEST,
@@ -21,5 +23,21 @@ export const fetchUsersFailure = (error) => {
   return {
     type: FETCH_USERS_FAILURE,
     payload: error,
+  };
+};
+
+export const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequest);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        const users = response.data;
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        dispatch(fetchUsersFailure(errorMessage));
+      });
   };
 };
